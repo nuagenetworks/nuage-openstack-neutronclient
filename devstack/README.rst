20b7fd0040397279c05d5f6e87f34575abfe3686
Vlad Gridin <Vladyslav.Gridin@alcatel-lucent.com>
2016-11-15 14:57:00 +0100

    Add devstack plugin support
    Change-Id: I5b91655fbbc26d8ec5b54c08ad8397953524f32b

---
+++ devstack/README.rst
@@ -0,0 +1,9 @@
+========================
+DevStack external plugin
+========================
+
+Add and set the following in your local.conf/localrc file:
+
+
+enable_plugin nuage-openstack-neutronclient https://github.com/nuage-networks/nuage-openstack-neutronclient.git
+
---
+++ devstack/plugin.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# plugin.sh - DevStack plugin.sh dispatch script
+
+DIR_NUAGECLIENT=$DEST/nuage-openstack-neutronclient
+
+function install_nuage_neutronclient {
+     setup_develop $DIR_NUAGECLIENT
+}
+
+
+
+# check for service enabled
+if is_service_enabled nuage_neutronclient; then
+
+    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of service source
+        echo_summary "Installing nuage-neutronclient"
+        install_nuage_neutronclient
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        :
+    fi
+fi
